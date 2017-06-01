75dafa4817287eab18eb3f8b5de7ed19569997b2
Prasad Putta <prasad.putta@azdoa.gov>
2017-05-31 17:36:13 -0700

    Prime numbers

---
+++ src/prasad/algorithms/PrimeNumber.java
@@ -0,0 +1,32 @@
+package prasad.algorithms;
+
+public class PrimeNumber {
+
+    public static void checkPrime(int n){
+
+        for(int i = 2;i< n;i++){
+            if(n % i == 0){
+                System.out.println(n + " is NOT a prime!!");
+                return;
+            }
+        }
+        System.out.println(n + " is a PRIME!!");
+
+    }
+
+    public static  void printPrimes(int n){
+
+        for (int i = 2;i<n;i++) {
+                checkPrime(i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+       // checkPrime(17);
+        printPrimes(15);
+
+    }
+
+}
